f9bd17be3d7b544179a976113edc7fb419c78c94
Innoviox <sarras305@gmail.com>
2017-10-28 11:24:06 -0400

    small change

--- view_test.py
+++ view_test.py
@@ -1,4 +1,4 @@
 from view.view import GridView
 
-grid = GridView(matrix=None) #creates default 20x20 matrix
+grid = GridView(matrix=None, dw=20, dh=20) #creates default 20x20 matrix
 grid.mainloop()
