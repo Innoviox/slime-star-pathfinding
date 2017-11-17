2d928a023b0cc4969ab1f5aac7d2e8d295326a94
Innoviox <sarras305@gmail.com>
2017-11-16 21:14:54 -0500

    simple usage

--- test.py
+++ test.py
@@ -1,7 +1,5 @@
 from pathfinding import AStar, file
-import time
 
 astar = AStar(*file.load_state_from_file('grid_save_2_21x21.txt'))
 for path in astar.find_path_iter():
-    print(path) 
- #   time.sleep(0.1)  
+    print(path, end='')
